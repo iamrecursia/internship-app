f3539ba8fbfe723692ba6c06c37580be5e224dfc
kirillKozitskiy <kkozickij35@gmail.com>
2026-02-09 04:26:35 +0300

    feat(user-service): update api paths, enable web support and optimize docker build

--- user-service/src/main/java/com/kozitskiy/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/kozitskiy/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.kozitskiy.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class UserServiceApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/kozitskiy/userservice/controller/CardController.java
+++ user-service/src/main/java/com/kozitskiy/userservice/controller/CardController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/cards")
+@RequestMapping("/api/v1/cards")
 @RequiredArgsConstructor
 public class CardController {
     private final CardService cardService;
